7b2bb98ae79b80c8d2928e1623af18ba2378c189
e-tae-hyeon <johtta888@naver.com>
2025-05-20 12:12:06 +0900

    feat: 수정 main using by react

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./main.ts", // 어떤 파일을 진입점으로 번들링할지
+  entry: "./main.tsx", // 어떤 파일을 진입점으로 번들링할지
   output: {
     filename: "bundle.js", // 번들로 만들어질 파일 이름
     path: path.resolve(__dirname, "dist"), // 번들 파일이 어디에 저장될지
